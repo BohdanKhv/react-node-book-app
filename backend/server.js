6b30c2b13ec155d48a05ff8589b4c32754eb4681
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-03-17 22:03:12 -0400

    deploy to heroku

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,15 @@ app.use(express.urlencoded({ limit: '1mb', extended: false }))
 // Routes
 app.use('/api/books', require('./routes/bookRoutes'))
 
+// Serv frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
+
 app.listen(5000, () => {
     console.log("Server is runnig")
 })
